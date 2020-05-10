feb190ad1fb4d910873685c69a80cc27cd1ed8a9
arthur rankin smith <arthurrankin1992@gmail.com>
2020-05-10 22:03:02 +0000

    learning all the things

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-print('printing text')
+# name = input("what is your name? ")
+# print('What up ' + name)
+
+# print(type(6))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4))
+
+# # math functions
+# print(round(3.0))
+# print(abs(-20))
+
+# print(20 - 3 * 4)
+
+# # variables 
+# iq = 190
+# print(iq)
+# user_iq = 190
+# print(user_iq)
+
+# some_value = 5
+# some_value += 2
+
+# print(some_value)
+
+# weather = "\t It\'s \"kind of\" sunny \n hope you have a good day"
+# print(weather)
+
+# username = input('usuername: ')
+# password = input('password: ')
+# password_length = len(password)
+# hidden_password = '*' * password_length
+
+# print(f'{username}, your password {hidden_password} is {password_length} letters long')
+
+# name = 'Arthur Smith'
+# age = 50
+# relationship_status = 'single'
+
+# relationship_status = 'it\'s complicated'
+
+# print(relationship_status)
+# import datetime
+
+# birth_year = input('what year were you born? ')
+# current_year = datetime.datetime.now().year
+# users_age = current_year - int(birth_year)
+
+
+# print(f'you are {users_age} years old')
+
+# lists
+li = [1,2,3,4,5]
+li2 = ['a', 'b', 'c']
+li3 = [1,2,'a', True]
